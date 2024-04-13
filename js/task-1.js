5bb6a5d2c71957473d213084c7d798af8617a0b2
Sveta28U <152779991+Sveta28U@users.noreply.github.com>
2024-04-13 12:51:38 +0300

    add 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const totalPrice = (quantity * pricePerDroid);
-    if(customerCredits)
+    if (makeTransaction <= customerCredits); 
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 console.log(makeTransaction(5, 3000, 23000)); 
