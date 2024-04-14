78ad7283075c982e37307e31c2f724f8305d6247
Sveta28U <152779991+Sveta28U@users.noreply.github.com>
2024-04-14 20:06:10 +0300

    add 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function checkForSpam(message) {
-const 
+  const str = message.toLowerCase();
+  return str.includes("sale") || str.includes("spam");
 }
-
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
